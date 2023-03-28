98b21e14d187aa4eb402a432a87e347a5bf6ba31
Jinkogule <lucaspimenta21@gmail.com>
2023-03-28 11:21:28 -0300

    teste

--- config/filesystems.php
+++ config/filesystems.php
@@ -1,5 +1,6 @@
 <?php
-
+use Aws\Credentials\Credentials;
+use Aws\S3\S3Client;
 return [
 
     /*
@@ -13,7 +14,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DISK', 'local'),
+    'default' => env('FILESYSTEM_DRIVER', 's3'),
 
     /*
     |--------------------------------------------------------------------------
@@ -22,7 +23,7 @@ return [
     |
     | Here you may configure as many filesystem "disks" as you wish, and you
     | may even configure multiple disks of the same driver. Defaults have
-    | been set up for each driver as an example of the required values.
+    | been setup for each driver as an example of the required options.
     |
     | Supported Drivers: "local", "ftp", "sftp", "s3"
     |
@@ -33,7 +34,6 @@ return [
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
-            'throw' => false,
         ],
 
         'public' => [
@@ -41,21 +41,19 @@ return [
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
-            'throw' => false,
         ],
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
+            'key' => 'AKIARA2BDGJ4IIZ4GBMG',
+            'secret' => '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',
+            'region' => 'sa-east-1',
+            'bucket' => 'adocaocriancas-laravel-imagens',
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'throw' => false,
+            'visibility' => 'public'
         ],
-
     ],
 
     /*
